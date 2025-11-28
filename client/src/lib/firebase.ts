c52f7b31990ad15dd0fb76a16edb9716bf73ca23
S.Mono <g36_orakul1109@yahoo.co.jp>
2025-11-28 10:38:53 +0900

    Firebaseの認証情報を更新

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 
 // Firebase Consoleで取得したconfigを貼り付け
 const firebaseConfig = {
-    apiKey: "AIzaSyCJWmgozIBHbzEmtJq1EH6aAa1g5gQtGf4",
-    authDomain: "booking-system-firebase-764d2.firebaseapp.com",
-    projectId: "booking-system-firebase-764d2",
-    storageBucket: "booking-system-firebase-764d2.firebasestorage.app",
-    messagingSenderId: "829906230754",
-    appId: "1:829906230754:web:6b51fa2c7f184e8788edbb"
+  apiKey: "AIzaSyCJWmgozIBHbzEmtJq1EH6aAa1g5gQtGf4",
+  authDomain: "booking-system-firebase-764d2.firebaseapp.com",
+  projectId: "booking-system-firebase-764d2",
+  storageBucket: "booking-system-firebase-764d2.firebasestorage.app",
+  messagingSenderId: "829906230754",
+  appId: "1:829906230754:web:6b51fa2c7f184e8788edbb"
 };
-
 // 初期化
 const app = initializeApp(firebaseConfig)
 
